0a42738dfa5058d93bb963a571ba0c95efbfa5e0
August Conwell <august.conwell@gmail.com>
2018-03-08 00:28:27 -0500

    Use mongoose's suggested query syntax

--- controllers/auth/passport/login.js
+++ controllers/auth/passport/login.js
@@ -4,26 +4,26 @@ const LocalStrategy = require('passport-local').Strategy;
 const USERNAME_PASSWORD_ERR = 'Incorrect username or password.'
 
 // User model
-const {Rep} = require('../../../models/Rep');
+const { Rep } = require('../../../models/Rep');
 
 module.exports = new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    Rep.findOne({ email: email }, (err, rep) => {
-        if (err) { return done(err); }
+    const query = Rep.where({ email })
+    query.findOne((err, rep) => {
+            if (err) { return done(err); }
 
-        // Invalid email 
-        if (!rep) {
-            return done(null, false, { message: USERNAME_PASSWORD_ERR });
-        }
-
-        rep.validatePassword(password, (isValid) => {
-
-            // Invalid password
-            if (!isValid) {
+            // Invalid email 
+            if (!rep) {
                 return done(null, false, { message: USERNAME_PASSWORD_ERR });
             }
+            rep.validatePassword(password, (isValid) => {
 
-            // Successful login attempt
-            return done(null, rep); // Success
+                // Invalid password
+                if (!isValid) {
+                    return done(null, false, { message: USERNAME_PASSWORD_ERR });
+                }
+
+                // Successful login attempt
+                return done(null, rep); // Success
+            })
         })
-    })
-});
+})
