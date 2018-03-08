b1b981659f765d2ee14b8f35062a3ec3506b347b
August Conwell <august.conwell@gmail.com>
2018-03-08 00:29:02 -0500

    First steps of first try on socket authentication

--- socket-server/index.js
+++ socket-server/index.js
@@ -2,10 +2,14 @@ const socket_io = require('socket.io');
 
 module.exports = (server) => {
     const io = socket_io(server)
-
+    io.use((socket, next) => {
+        console.log(socket.request)
+        if (socket.request.headers.cookie) return next();
+        next(new Error('Authentication error'));
+    })
     io.on('connection', (socket) => {
         console.log('new connection')
-        
+        console.log(socket);
         socket.on('chat message', (socket) => {
             console.log(socket)
         })
